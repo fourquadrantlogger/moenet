3adafa4ebbb298a216f823d66a3b74b58a56c61d
timeloveboy <734991033@qq.com>
2017-04-17 20:21:14 +0800

    ~

--- BrowserState.go
+++ BrowserState.go
@@ -1,21 +1,36 @@
 package moenet
 
-import "net/url"
+import (
+	"encoding/json"
+	"net/url"
+)
 
 type BrowserState struct {
 	cookies *MemoryCookieStorage
 	history []*requestlog
 }
 
-func (this *BrowserState)AddReqlog(req requestlog){
-	this.history=append(req)
+func (this *BrowserState) AddReqlog(req requestlog) {
+	this.history = append(req)
 }
-func (this *BrowserState)GetCookies()(*MemoryCookieStorage){
+func (this *BrowserState) GetCookies() *MemoryCookieStorage {
 	return this.cookies
 }
-func (this *BrowserState)NowUrl()(*url.URL){
-	if(len(this.history)==0){
+func (this *BrowserState) NowUrl() *url.URL {
+	if len(this.history) == 0 {
 		return nil
 	}
 	return this.history[len(this.history)-1].requrl
+}
+func (this *BrowserState) String() string {
+	var js map[string]interface{}
+	js["cookies"] = this.cookies
+	js["history"] = this.history
+
+	bs, _ := json.Marshal(js)
+	return string(bs)
+}
+func ParseBrowserState(s string) (this *BrowserState) {
+	json.Unmarshal([]byte(s), this)
+	return
 }
